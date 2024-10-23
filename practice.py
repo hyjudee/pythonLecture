19985b667fed5d0f4fb8a709a262ec2db0d18417
hyjudee <hyeju.wooo@gmail.com>
2024-10-23 17:27:04 +0900

    Update practice.py
    commented if statement for "temperature conversion"

--- practice.py
+++ practice.py
@@ -92,11 +92,11 @@
 unit = input("is this temperature in celsius or fahrenheit? (C/F): ")
 temp = float(input("enter the temp: "))
 
-if unit == "C":
-    temp = round((9 * temp) / 5 + 32, 1)
-    print(f"the temp in °F is {temp}")
-elif unit == "F":
-    temp = round((temp - 32) * 5 / 9, 1)
-    print(f"the temp in °C is {temp}")
-else:
-    print(f"{unit} is an invalid unit of measurement")
+# if unit == "C":
+#     temp = round((9 * temp) / 5 + 32, 1)
+#     print(f"the temp in °F is {temp}")
+# elif unit == "F":
+#     temp = round((temp - 32) * 5 / 9, 1)
+#     print(f"the temp in °C is {temp}")
+# else:
+#     print(f"{unit} is an invalid unit of measurement")
