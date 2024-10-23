cf832fe5aa660fa99df6d90ad8fcd0fe729d562d
hyjudee <hyeju.wooo@gmail.com>
2024-10-23 13:48:54 +0000

    conditional expresstion

--- practice.py
+++ practice.py
@@ -1,64 +1,69 @@
 # exercise 1 rectangle area calc
-#length = float(input("Enter the length: "))
-#width = float(input("Enter the width: "))
-#area = length * width
+# length = float(input("Enter the length: "))
+# width = float(input("Enter the width: "))
+# area = length * width
 
-#print(f"The area is: {area}cmª")
+# print(f"The area is: {area}cmª")
 
 # exercise 2 shopping cart program
-#item = input("what iteam would you like to buy?: ")
-#price = float(input("what is the price?: "))
-#quantity = int(input("how many would you like?: "))
-#total = price * quantity
+# item = input("what iteam would you like to buy?: ")
+# price = float(input("what is the price?: "))
+# quantity = int(input("how many would you like?: "))
+# total = price * quantity
 
-#print(total)
-#print(f"your total price is: ${total}")
+# print(total)
+# print(f"your total price is: ${total}")
 
-#madlibs game
-#word game where you can create a story
-#by filling in blanks with random words
-#adjective1 = input("enter an adjective: ")
-#noun1 = input("enter a place: ")
-#adjective2 = input("enter what you were doing: ")
-#adjective3 = input("enter how you felt about it: ")
 
-#print(f"Today i went to a {adjective1} {noun1}")
-#print(f"in the {noun1}, I was {adjective2}")
-#print(f"{adjective2} was {adjective3}")
+# madlibs game
+# word game where you can create a story
+# by filling in blanks with random words
+# adjective1 = input("enter an adjective: ")
+# noun1 = input("enter a place: ")
+# adjective2 = input("enter what you were doing: ")
+# adjective3 = input("enter how you felt about it: ")
 
-#import math
-#radius = float(input("enter the radius a circle: "))
-#circumference = 2 * math.pi * radius
-#print(f"The circumference is : {round(circumference, 2)}cm")
+# print(f"Today i went to a {adjective1} {noun1}")
+# print(f"in the {noun1}, I was {adjective2}")
+# print(f"{adjective2} was {adjective3}")
 
-#radius = float(input("enter the radius: "))
-#area = math.pi * pow(radius, 2)
-#print(f"the area is: {round(area, 2)}cm")
 
-#a = float(input("enter side a: "))
-#b = float(input("enter side b: "))
-#c = math.sqrt(pow(a, 2) + pow(b, 2))
+# import math
+# radius = float(input("enter the radius a circle: "))
+# circumference = 2 * math.pi * radius
+# print(f"The circumference is : {round(circumference, 2)}cm")
 
-#print(f"the side c is {c}")
-#print(f"the side c is {round(c, 2)}")
+# radius = float(input("enter the radius: "))
+# area = math.pi * pow(radius, 2)
+# print(f"the area is: {round(area, 2)}cm")
 
-#age = int(input("enter your age: "))
-#if age >= 18:
+# a = float(input("enter side a: "))
+# b = float(input("enter side b: "))
+# c = math.sqrt(pow(a, 2) + pow(b, 2))
+
+# print(f"the side c is {c}")
+# print(f"the side c is {round(c, 2)}")
+
+
+# age = int(input("enter your age: "))
+# if age >= 18:
 #    print("you are now signed up!")
-#elif age < 0:
+# elif age < 0:
 #    print("you haven't been born yet") 
-#else:
+# else:
 #    print("you must be 18+ to sign up!")
 
-#response = input("would you like food? (Y/N): ")
-#if response == "Y":
+
+# response = input("would you like food? (Y/N): ")
+# if response == "Y":
 #    print("i like you too")
-#else:
+# else:
 #    print("i don't like you either")
 
-#operator = input("enter an operator (+, -, *, /): ")
-#num1 = float(input("enter the 1st number: "))
-#num2 = float(input("enter the 2nd number: "))
+
+# operator = input("enter an operator (+, -, *, /): ")
+# num1 = float(input("enter the 1st number: "))
+# num2 = float(input("enter the 2nd number: "))
 
 # if operator == "+":
 #    result = num1 + num2
@@ -75,6 +80,7 @@
 # else:
 #    print(f"{operator} is not a valid operator")
 
+
 # weight = float(input("enter your weight: "))
 # unit = input("kg or pound? (K / L): ")
 
@@ -89,6 +95,7 @@
 # else:
 #     print(f"{unit} is not a valid")
 
+
 # unit = input("is this temperature in celsius or fahrenheit? (C/F): ")
 # temp = float(input("enter the temp: "))
 
@@ -100,6 +107,7 @@
 #     print(f"the temp in °C is {temp}")
 # else:
 #     print(f"{unit} is an invalid unit of measurement")
+
 
 # temp = 25
 # is_raining = False
@@ -129,3 +137,31 @@
 # elif 28 > temp > 0 and not is_sunny:
 #     print("it's wamr outside")
 #     print("it's cloudy")
+
+
+# num = 5
+# num = int(input("enter a number: "))
+# # print("postive" if num > 0 else "negative")
+
+# result = "even" if num % 2 == 0 else "odd"
+# print(result)
+
+# a = 6
+# b = 7
+# max_num = a if a > b else b
+# min_num = a if a < b else b
+# print(max_num)
+# print(min_num)
+
+# age = 25
+# age = int(input("enter your age: "))
+# status = "adult" if age >= 18 else "child"
+# print(status)
+
+# temp = 20
+# weather = "hot" if temp > 25 else "cold"
+# print(weather)
+
+# user_role = "admin"
+# access_level = "full access" if user_role == "admin" else "limited access"
+# print(access_level)
