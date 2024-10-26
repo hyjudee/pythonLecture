f453e9c65d78bac2509a780ecd2b90a2dece7809
hyjudee <hyeju.wooo@gmail.com>
2024-10-27 00:29:35 +0900

    compoundInterestCalculator

--- practice.py
+++ practice.py
@@ -245,4 +245,45 @@ num = int(input("enter a # between 1 - 10: "))
 while num < 1 or num > 10:
     print(f"{num} is not vaild")
     num = int(input("enter a # bewteen 1 - 10: "))
-print(f"your number is {num}")
+print(f"your number is {num}")principle = 0
+rate = 0
+time = 0
+
+# while principle <= 0:
+#     principle = float(input("enter the principle amount: "))
+#     if principle <= 0:
+#         print("it can not be less than or equal to zero")
+
+# while rate <= 0:
+#     rate = float(input("enter the interest rate: "))
+#     if rate <= 0:
+#         print("it can not be less than or equal to zero")
+
+# while time <= 0:
+#     time = int(input("enter the time in years: "))
+#     if time <= 0:
+#         print("it can not be less than or equal to zero")
+
+while True:
+    principle = float(input("enter the principle amount: "))
+    if principle <= 0:
+        print("it can not be less than or equal to zero")
+    else:
+        break   
+
+while True:
+    rate = float(input("enter the interest rate: "))
+    if rate <= 0:
+        print("it can not be less than or equal to zero")
+    else:
+        break    
+
+while True:
+    time = int(input("enter the time in years: "))
+    if time <= 0:
+        print("it can not be less than or equal to zero")
+    else:
+        break
+
+total = principle * pow((1 + rate / 100), time)
+print(f"balance after {time} year/s: ${total:.2f}")
