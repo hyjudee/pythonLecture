04606ceabc469f133180070c66fbdbfb4a27ee6d
hyjudee <hyeju.wooo@gmail.com>
2024-10-27 03:24:44 +0900

    countdownTimer

--- practice.py
+++ practice.py
@@ -309,4 +309,13 @@ while True:
         break
 
 total = principle * pow((1 + rate / 100), time)
-print(f"balance after {time} year/s: ${total:.2f}")
+print(f"balance after {time} year/s: ${total:.2f}")import time
+my_time = int(input("enter the time in second: "))
+
+for x in range(my_time, 0, -1):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+print("time's up")
