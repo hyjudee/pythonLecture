b5d2c8bd63ebd646c9b4cdd32e79ec836845a7eb
hyjudee <hyeju.wooo@gmail.com>
2024-10-27 13:37:50 +0000

    nestedLoop

---
+++ 20-nestedLoop.py
@@ -0,0 +1,13 @@
+# for x in range(3):
+#     for y in range(1, 6):
+#         print(y, end="")
+#     print(y, end=",")    
+
+# rows = int(input("enter the num of rows: "))
+# columns = int(input("enter the num of columns: "))
+# symbols = input("enter a symbol to use: ")
+
+# for x in range(rows):
+#     for y in range(columns):
+#         print(symbols, end="")
+#     print()
