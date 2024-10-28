e608b8290006523c92517106176c2766829d710d
hyjudee <hyeju.wooo@gmail.com>
2024-10-28 08:16:27 +0000

    collection
    list, set, tuple

--- 20-nestedLoop.py
+++ 20-nestedLoop.py
@@ -1,7 +1,7 @@
 # for x in range(3):
 #     for y in range(1, 6):
 #         print(y, end="")
-#     print(y, end=",")    
+#     print(end=",")    
 
 # rows = int(input("enter the num of rows: "))
 # columns = int(input("enter the num of columns: "))
@@ -11,3 +11,4 @@
 #     for y in range(columns):
 #         print(symbols, end="")
 #     print()
+
---
+++ 21-collection.py
@@ -0,0 +1,31 @@
+# fruits = ["apple", "orange", "banana"]
+# fruits = {"apple", "orange", "banana"}
+fruits = ("apple", "orange", "banana")
+
+# print(fruits[:2])
+# print(len(fruits))
+# print(dir(fruits))
+# print(help(fruits))
+# print("apple" in fruits)
+
+# fruits[0] = "pineapple"
+# fruits.append("pineapple")
+# fruits.remove("apple")
+# fruits.insert(0, "pineapple")
+# fruits.sort()
+# fruits.reverse()
+# fruits.clear()
+
+# fruits.add("pineapple")
+# fruits.remove("apple")
+# fruits.pop()
+
+# print(fruits.index("apple"))
+# print(fruits.count("apple"))
+
+
+# print(fruits)
+
+
+# for fruit in fruits:
+#     print(fruit)
