edf8d2b8af6a402fde7539935a84dc613aaa2523
hyjudee <hyeju.wooo@gmail.com>
2024-10-29 18:11:06 +0000

    random numbers

---
+++ 27-randomNumbers.py
@@ -0,0 +1,19 @@
+import random
+# print(help(random))
+
+# print(random.randint(1, 6))
+
+# low = 1
+# high = 100
+
+# number = random.randint(low, high)
+# number = random.random()
+
+# options = ("rock", "paper", "scissors")
+# option = random.choice(options)
+
+# cards = ["2", "3", "4", "5", "q", "k", "j"]
+# random.shuffle(cards)
+
+# print(cards)
+
