8fe841ebcb5b99b7a52e0fc3e621bc48092fe103
hyjudee <hyeju.wooo@gmail.com>
2024-10-30 19:06:46 +0000

    functions
    def, return

---
+++ 31-functions.py
@@ -0,0 +1,53 @@
+# def happy_bday():
+#     print("happy bday to you")
+#     print("you are old")
+#     print("happy bday!")
+#     print()
+
+# def happy_bday(name):
+#     print(f"happy bday to {name}")
+#     print("you are old")
+#     print("happy bday!")
+#     print()
+
+# def happy_bday(name, age):
+#     print(f"happy bday to {name}")
+#     print(f"you are {age} years old")
+#     print("happy bday!")
+#     print()
+# happy_bday("ju", 30)
+
+# def display_invoice(username, amount, due_date):
+#     print(f"hello, {username}!")
+#     print(f"your bill of ${amount:.2f} is due: {due_date}")
+# display_invoice("ju", 50, "31/10")
+
+
+# def add(x,y):
+#     z = x + y
+#     return z
+
+# def subtract(x,y):
+#     z = x - y
+#     return z
+
+# def multiply(x,y):
+#     z = x * y
+#     return z
+
+# def divide(x,y):
+#     z = x / y
+#     return z
+
+# print(add(4,5))
+# print(subtract(4,5))
+# print(multiply(4,5))
+# print(divide(4,5))
+
+
+# def create_name(first, last):
+#     first = first.capitalize()
+#     last = last.capitalize()
+#     return first + " " + last
+# full_name = create_name("jeremy", "ju")
+# print(full_name)
