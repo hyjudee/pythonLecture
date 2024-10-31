630b9fbfe8d4f255c78948bcca4e15290cb71b99
hyjudee <hyeju.wooo@gmail.com>
2024-10-31 12:00:43 +0000

    default argument

---
+++ 32-defaultArgument.py
@@ -0,0 +1,17 @@
+# def net_price(list_price, discount=0, tax=0.05):
+#     return list_price * (1 - discount) * (1 + tax)
+
+# print(net_price(500))
+# print(net_price(500, 0.2))
+# print(net_price(500, 0.2, 0))
+
+# import time
+
+# def count(end, start=0):
+#     for x in range(start, end+1):
+#         print(x)
+#         time.sleep(1)
+#     print("done!")
+
+# # count(5)
+# count(10, 5)
