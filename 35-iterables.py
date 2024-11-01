548ff6b079dca80f27f5f86babde15acec9ca400
hyjudee <hyeju.wooo@gmail.com>
2024-11-01 16:47:15 +0000

    iterables
    list&tuple, set, string, dictionaries

---
+++ 35-iterables.py
@@ -0,0 +1,21 @@
+numbers = [1, 2, 3, 4, 5]
+# for number in numbers:
+#     print(number)
+# for number in reversed(numbers):
+#     print(number)
+
+fruits = {"apple", "banana", "orange"}
+# for fruit in fruits:
+#     print(fruit)
+
+name = "hj woo"
+# for character in name:
+#     print(character, end=" ")
+
+my_dictionary = {"a": 1, "b": 2, "c":3}
+# for key in my_dictionary:
+#     print(key)
+# for value in my_dictionary.values():
+#     print(value)
+for key, value in my_dictionary.items():
+    print(f"{key} = {value}")
