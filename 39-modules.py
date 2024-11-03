b9a8e2a5fb3df600277300f0c92125c04539dad4
hyjudee <hyeju.wooo@gmail.com>
2024-11-03 13:07:08 +0000

    modules

---
+++ 39-modules.py
@@ -0,0 +1,19 @@
+# print(help("modules"))
+
+# import math
+# print(math.pi) 
+
+# import math as m
+# print(m.pi)
+
+# from math import pi
+# print(pi)
+
+# from math import e
+a, b, c, d, e = 1, 2, 3, 4, 5
+# print(e ** a)
+# print(e ** e)
+
+import math
+print(math.e ** a)
+print(math.e ** e)
