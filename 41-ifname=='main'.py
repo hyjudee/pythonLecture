512668250010db4c368c1821c9c5bc51c98f1e08
hyjudee <hyeju.wooo@gmail.com>
2024-11-04 16:57:11 +0000

    if __name__ == '__main__'

---
+++ 41-ifname=='main'.py
@@ -0,0 +1,26 @@
+#script1
+def favourite_food(food):
+    print(f"your favourite food is {food}")
+
+def main():
+    print("this is script1")
+    favourite_food("pizza")
+    print("goodbye")
+
+if __name__ == '__main__':
+    main()
+
+
+#script2
+def favourite_drink(drink):
+    print(f"your favourite drink {drink}")
+
+def main():
+    print("this is script2")
+    favourite_food("shushi")
+    favourite_drink("water")
+    print("bye")
+
+if __name__ == '__main__':
+    main()
+    
