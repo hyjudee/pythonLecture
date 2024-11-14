de4d96471e7d65d130a49d3781908f7310ee73e3
hyjudee <hyeju.wooo@gmail.com>
2024-11-14 07:52:08 +0000

    PyQt5 GUI intro

---
+++ 66-PyQt5GUIIntro.py
@@ -0,0 +1,22 @@
+import sys
+from PyQt5.Qtwidgets import QApplication, QMainWindow
+from PyQt5.QtGui import Qicon
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWidonwTitle("my cool first GUI")
+        self.setGeometry(0, 0, 500, 500)
+        self.setWindowIcon(Qicon("pic.jpg"))
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
+
+
+
