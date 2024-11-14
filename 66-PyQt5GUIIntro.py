e6b1bde8d1648540064ad35284f38dc13f848177
hyjudee <hyeju.wooo@gmail.com>
2024-11-14 08:37:54 +0000

    modified

--- 66-PyQt5GUIIntro.py
+++ 66-PyQt5GUIIntro.py
@@ -1,13 +1,13 @@
 import sys
 from PyQt5.Qtwidgets import QApplication, QMainWindow
-from PyQt5.QtGui import Qicon
+from PyQt5.QtGui import QIcon
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWidonwTitle("my cool first GUI")
         self.setGeometry(0, 0, 500, 500)
-        self.setWindowIcon(Qicon("pic.jpg"))
+        self.setWindowIcon(QIcon("pic.jpg"))
 
 def main():
     app = QApplication(sys.argv)
